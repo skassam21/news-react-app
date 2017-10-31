253218c8ce6b8e88248dc25e9ae0135525fffb04
Shums Kassam <skassam21@gmail.com>
2017-10-31 09:45:23 -0400

    intro question

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import s from './App.scss';
 
-class App extends Component {
+function IntroMessage(props) {
+  return (
+    <div className="row">
+      <div className="col-lg-12">
+        <h1>Welcome to Newsboard!</h1>
+        <button className="btn btn-primary"
+          style={{marginTop: '20px'}}>Get Started</button>
+      </div>
+    </div>
+  )
+}
 
-  render() {
-    return <h1>Hello, React!</h1>;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: 1
+    }
   }
 
+  render() {
+    let question = null;
+    if (this.state.question === 1) {
+      question = <IntroMessage />;
+    }
+
+    let styles = {
+      textAlign: 'center',
+      paddingTop: '200px'
+    }
+
+    return (
+      <div className="intro-page container" style={styles}>
+        {question}
+      </div>
+    )
+  }
 }
 
 export default App;
