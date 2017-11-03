fac93301dc1a09b36f40ef4904c51cf3e1a8948b
Starfoil <dam.jason@hotmail.com>
2017-11-02 21:45:25 -0400

    Made dynamic questions

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import LandingPage from './../LandingPage/LandingPage';
-import FirstQuestion from './../Questions/Questions';
+import FirstQuestion from './../Questions/FirstQuestion';
+import SecondQuestion from './../Questions/SecondQuestion';
+import ThirdQuestion from './../Questions/ThirdQuestion';
 import s from './App.scss';
 
 
@@ -8,9 +10,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 'landingPage'
+      page: 'landingPage',
+      articles: [],
+      sources: []
     }
 
+    this.addSources = (newSources) => this._addSources(newSources);
     this.changePage = (newPage) => this._changePage(newPage);
   }
 
@@ -20,13 +25,25 @@ class App extends Component {
     })
   }
 
+  _addSources(newSources) {
+	//console.log(newSources);
+	for (var i = 0; i < newSources.length; i++) {
+		this.state.sources.push(newSources[i]);
+	}
+	console.log(this.state.sources);
+  }
+
   render() {
     let pageView = null;
 
     if (this.state.page === 'landingPage') {
       pageView = <LandingPage changePage={this.changePage} />;
     } else if (this.state.page === 'firstQuestion') {
-      pageView = <FirstQuestion />;
+      pageView = <FirstQuestion changePage={this.changePage} addSources={this.addSources}/>;
+    } else if (this.state.page === 'secondQuestion') {
+      pageView = <SecondQuestion changePage={this.changePage} addSources={this.addSources}/>;
+    } else if (this.state.page === 'thirdQuestion') {
+      pageView = <ThirdQuestion changePage={this.changePage} addSources={this.addSources}/>;
     }
 
     let styles = {
